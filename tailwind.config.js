bae26b461edd3f424ddee334ffa41800f7baf8e0
mhannibal <orthysolution@gmail.com>
2022-04-09 18:00:41 +0100

    new styles , project page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'body': ['Quicksand', 'sans-serif',],            
+      'title': ['Anton', ],
+    },
+    colors: {
+      secondary: {
+        200:'#25262d',
+        100:'#4e7791',
+      },
+      white : {
+        100: '#FFFFFF',
+      },
+      gray : {
+        100: '#3e3e3e ',
+      },
+      black : {
+        100: '#000000',
+      },
+    }
   },
   plugins: [],
 }
